3b87fe7cf38320ebdef5bf07b1c931f616f1c05a
Frizzione <ferla.damiano@gmail.com>
2023-06-17 15:14:08 +0200

    Update PatchCore.py

--- PatchCore.py
+++ PatchCore.py
@@ -21,12 +21,13 @@ class PatchCore(torch.nn.Module):
     self.k = k                                                                          # HYPERPARAMETER
     self.sigma = sigma
     self.kernel_size = int(2 * self.sigma * 4 + 1) 
+    self.feature_extractor = timm.create_model(backbone, pretrained=True, features_only=True, out_indices=self.out_indices)
     if torch.cuda.is_available():
         self.device = 'cuda'
         self = self.to(self.device)
+        self.feature_extractor = self.feature_extractor.to(self.device)
     else:
         self.device = 'cpu'
-    self.feature_extractor = timm.create_model(backbone, pretrained=True, features_only=True, out_indices=self.out_indices)
     for param in self.feature_extractor.parameters():
       param.requires_grad = False
     self.feature_extractor.eval()                                                       # Inference mode instead of training
